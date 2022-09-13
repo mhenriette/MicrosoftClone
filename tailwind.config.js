4248db46804c481e137f30383a68e4c7d50a118f
Henriette <mhenriette00@gmail.com>
2022-09-13 09:30:48 +0200

    add responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                newgray: '#f2f2f2'
+            }
+        },
+    },
+    plugins: [],
+}
